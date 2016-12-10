bc16b1dc1b1980bd8e18ab2f7acbd0fffc503bea
DmitriyStasenko <kpi22526@gmail.com>
2016-12-10 15:37:01 +0200

    Commit

--- src/Module5/homework/Main.java
+++ src/Module5/homework/Main.java
@@ -1,5 +1,7 @@
 package Module5.homework;
 
+import java.util.Date;
+
 /**
  * Created by Дима on 09.12.2016.
  */
@@ -7,6 +9,27 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+   Controller controller = new Controller();
+
+        controller.requstRooms(1000,2,"Kiev","Hotel1");
+        controller.requstRooms(600,1,"Kharkiv","Hotel2");
+        for(Room a : controller.requstRooms(1000,2,"Kiev","Hotel1")){
+            System.out.println(a.getId());
+        }
+
+        API ap = new BookingComAPI();
+        API ap2 = new GoogleAPI();
+        controller.check(ap,ap2);
+
+
+        Room b = new Room(5,1000,2, new Date(),"Hotel20","Lugansk");
+        DAO dao = new DAOheir();
+        dao.save(b);
+        dao.delete(b);
+
+
+
 
     }
 
